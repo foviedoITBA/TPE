d2f77d17526f0e8af74f1c026d7d19ec2562b09a
Fernán <foviedo@itba.edu.ar>
2015-10-15 12:31:46 -0300

    tipos básicos

--- 2048Back.h
+++ 2048Back.h
@@ -1,1 +1,19 @@
 #include <stdlib.h>
+#include <time.h>
+
+#define fichaNueva ((rand() / (double) RAND_MAX) <= 0.89 ? 2 : 4)
+
+typedef unsigned short int FICHA
+typedef **FICHA TABLERO
+typedef unsigned short int DIFICULTAD
+typedef unsigned int PUNTAJE
+
+typedef struct
+{
+	DIFICULTAD dificultad;
+	PUNTAJE puntaje;
+	char undos;
+	TABLERO tablero;
+} INFO;
+
+
